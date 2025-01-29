ce6ce93d9f59de511f9808b1cc64d6cc2252c0cd
giyuer <giyuucarousell@gmail.com>
2025-01-30 05:32:57 +0800

    Update bounty.lua

--- bounty.lua
+++ bounty.lua
@@ -15,7 +15,7 @@ getgenv().config = {
     ["SkipFruit"] = {
         "Portal-Portal",
     },
-    ["Skip Race V4 User"] = false,
+    ["Skip Race V4 User"] = true,
     ["Team"] = "Pirates",
     ["MaxBountyHunt"] = 30000000,
     ["MinBountyHunt"] = 0,
