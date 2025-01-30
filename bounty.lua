8b1f8054819e2a521053bdcf2cd7839b1579ebfe
giyuer <giyuucarousell@gmail.com>
2025-01-30 23:35:13 +0800

    Update bounty.lua

--- bounty.lua
+++ bounty.lua
@@ -1,138 +1,98 @@
-getgenv().Configs = {
-    ["Team"] = "Pirates", -- Pirates/Marines
-    ["Speed Tween"] = 100, -- Highly recommend 300 speed is good enough to anti security kick. maximum you can use is 350
-    ["Auto turn on race v3"] = true,
-    ["Auto turn on race v4"] = true,
-    ["Click Time"] = 0.2,
-    ["White Screen"] = false, -- false / true
-    ["Chat"] = {
-        ["Enable"] = false,
-        ["Message"] = {"Made By Night", "Made By DarkRel"}, -- Input your message here
+getgenv().key = "7af34W9DWolt7w1CBL0R7mgJw"
+getgenv().config = {
+    ["Use Race"] = {
+        ["V4"] = true,
+        ["V3"] = true,
     },
-    ["Run When Low Health"] = {
-        ["Enable"] = true,
-        ["Health"] = 3500,
-        ["Come back"] = true
+    ["BypassTp"] = false,
+    ["Info Screen"] = true,
+    ["SafeHealth"] = 5000,
+    ["Webhooks"] = {
+        ["Toggle Webhook"] = false,
+        ["Link Webhook"] = "",
     },
-    ["Region"] = "All", -- Singapore, United States, Germany, France, India, ??? ... or "All"
-    ["Weapons"] = {
-        ["Melee"] = {
+    ["White Screen"] = false,
+    ["SkipFruit"] = {
+        "Portal-Portal",
+    },
+    ["Skip Race V4 User"] = true,
+    ["Team"] = "Pirates",
+    ["MaxBountyHunt"] = 30000000,
+    ["MinBountyHunt"] = 0,
+    ["MainSkillToggle"] = {
+        ["Sword"] = {
             ["Enable"] = true,
-            ["Delay"] = 1.75,
-            ["Order"] = 3,
             ["Skills"] = {
-                ["Z"] = {
-                    ["Enable"] = true,
-                    ["HoldTime"] = 0,
-                    ["Order"] = 0.1,
-                    ["Time to next skill"] = 0
-                },
                 ["X"] = {
                     ["Enable"] = true,
-                    ["HoldTime"] = 0,
-                    ["Order"] = 0.5,
-                    ["Time to next skill"] = 0
+                    ["HoldTime"] = 0.5,
                 },
-                ["C"] = {
+                ["Z"] = {
                     ["Enable"] = true,
-                    ["HoldTime"] = 0,
-                    ["Order"] = 0.25,
-                    ["Time to next skill"] = 0
-                }
-            }
+                    ["HoldTime"] = 1.2,
+                },
+            },
+            ["Delay"] = 1.3,
         },
         ["Blox Fruit"] = {
             ["Enable"] = false,
-            ["Delay"] = 0.2,
-            ["Order"] = 4,
             ["Skills"] = {
-                ["Z"] = {
-                    ["Enable"] = true,
-                    ["HoldTime"] = 0,
-                    ["Order"] = 2,
-                    ["Time to next skill"] = 0
-                },
                 ["X"] = {
                     ["Enable"] = true,
-                    ["HoldTime"] = 0,
-                    ["Order"] = 1,
-                    ["Time to next skill"] = 0
+                    ["HoldTime"] = 2,
                 },
                 ["C"] = {
                     ["Enable"] = false,
                     ["HoldTime"] = 0,
-                    ["Order"] = 0,
-                    ["Time to next skill"] = 0
                 },
-                ["V"] = {
-                    ["Enable"] = false,
+                ["Z"] = {
+                    ["Enable"] = true,
                     ["HoldTime"] = 0,
-                    ["Order"] = 0,
-                    ["Time to next skill"] = 0
                 },
                 ["F"] = {
                     ["Enable"] = false,
                     ["HoldTime"] = 0,
-                    ["Order"] = 0,
-                    ["Time to next skill"] = 0
-                }
-            }
-        },
-        ["Sword"] = {
-            ["Enable"] = true,
-            ["Delay"] = 0.5,
-            ["Order"] = 1,
-            ["Skills"] = {
-                ["Z"] = {
-                    ["Enable"] = true,
-                    ["HoldTime"] = 0.35,
-                    ["Order"] = 0.5,
-                    ["Time to next skill"] = 0
                 },
+                ["V"] = {
+                    ["Enable"] = false,
+                    ["HoldTime"] = 0,
+                },
+            },
+            ["Delay"] = 2,
+        },
+        ["Melee"] = {
+            ["Enable"] = true,
+            ["Skills"] = {
                 ["X"] = {
                     ["Enable"] = true,
-                    ["HoldTime"] = 0.25,
-                    ["Order"] = 0.25,
-                    ["Time to next skill"] = 0
-                }
-            }
+                    ["HoldTime"] = 0.14,
+                },
+                ["C"] = {
+                    ["Enable"] = true,
+                    ["HoldTime"] = 0.4,
+                },
+                ["Z"] = {
+                    ["Enable"] = true,
+                    ["HoldTime"] = 1.2,
+                },
+            },
+            ["Delay"] = 1,
         },
         ["Gun"] = {
             ["Enable"] = false,
-            ["Delay"] = 1.5,
-            ["Order"] = 2,
             ["Skills"] = {
-                ["Z"] = {
-                    ["Enable"] = true,
-                    ["HoldTime"] = 0,
-                    ["Order"] = 2,
-                    ["Time to next skill"] = 0
-                },
                 ["X"] = {
                     ["Enable"] = true,
                     ["HoldTime"] = 0,
-                    ["Order"] = 1,
-                    ["Time to next skill"] = 0
-                }
-            }
-        }
+                },
+                ["Z"] = {
+                    ["Enable"] = true,
+                    ["HoldTime"] = 0,
+                },
+            },
+            ["Delay"] = 1,
+        },
     },
-    ["Theme"] = {
-        ["Enable"] = true,
-        ["Hide Theme"] = true,
-        ["Name"] = "Gojo", -- Gojo
-        ["Custom Theme"] = {
-            ["Enable"] = false, -- enable theme first
-            ["Text Color"] = Color3.fromRGB(231, 85, 88),
-            ["Character Position"] = UDim2.new(0.563000023, 0, -0.174999997, 0),
-            ["Character Size"] = UDim2.new(0.3, 0, 1.4, 0)
-        }
-    },
-    ["Webhook"] = {
-        ["Enable"] = false,
-        ["Url"] = "https://discord.com/api/webhooks/1330140123198652489/HDQDd4Aq3ABG_16nf3KjImn3gjZukiTXVomNgo8nyrHkP-wD15YnFKNEvxL5BDPFdnUH",
-        ["Image"] = "https://media.discordapp.net/attachments/1162947702615711765/1229739840661094451/background.png?ex=67576eb9&is=67561d39&hm=da94e1b066b7a974e1577fe4fb355ce2907de8acf56aa4c467d7023a59d1d927&=&format=webp&quality=lossless&width=692&height=395"
-    }
 }
 
-repeat task.wait(1) pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/Xero2409/XeroHub/refs/heads/main/bounty.lua"))() end) until getgenv().Execution
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LumosSera/Serra/refs/heads/main/Bounty.lua"))()
